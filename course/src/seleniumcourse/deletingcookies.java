aa79caa9b5849b777da4cc81df19760d2b92071b
bhanu velamuri <bhanu@dal.ca>
2018-08-10 14:22:57 -0300

    code for taking screnshots in selenium testing

--- course/src/seleniumcourse/deletingcookies.java
+++ course/src/seleniumcourse/deletingcookies.java
@@ -2,19 +2,28 @@ package seleniumcourse;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+
 
 public class deletingcookies {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		WebDriver  driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		//driver.manage().deleteCookieNamed("asdf");
+		driver.manage().deleteAllCookies(); // for deleting all cookies
+		//driver.manage().deleteCookieNamed("asdf"); for particular cookie
+		//driver.manage().deleteCookie("sesion key");// deleting session cookie
+		//click on any link it should redirect to login page
+		driver.get("http://google.com");
+		File src=    ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\Users\\bhanu\\screenshot.png"));
 		
-		
-
 	}
 
 }
