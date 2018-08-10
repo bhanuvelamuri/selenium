2ec1983fe434d3c29cd5eb75415c61cd19e6c032
bhanu velamuri <bhanu@dal.ca>
2018-08-10 14:32:21 -0300

    killing a windows process

--- course/src/seleniumcourse/deletingcookies.java
+++ course/src/seleniumcourse/deletingcookies.java
@@ -1,12 +1,13 @@
 package seleniumcourse;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
+import org.openqa.selenium.os.WindowsUtils;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class deletingcookies {
@@ -15,6 +16,7 @@ public class deletingcookies {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		WebDriver  driver=new ChromeDriver();
+		WindowsUtils.killByName("notepad.exe");
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies(); // for deleting all cookies
 		//driver.manage().deleteCookieNamed("asdf"); for particular cookie
@@ -23,6 +25,9 @@ public class deletingcookies {
 		driver.get("http://google.com");
 		File src=    ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(src, new File("C:\\Users\\bhanu\\screenshot.png"));
+		
+		//killing process 
+		
 		
 	}
 
