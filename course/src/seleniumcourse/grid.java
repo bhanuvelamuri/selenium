57734d6c3f14bcbf6e6f631290ec280c4b45a428
bhanu velamuri <bhanu@dal.ca>
2018-08-11 01:43:30 -0300

    supporting docs

---
+++ course/src/seleniumcourse/grid.java
@@ -0,0 +1,12 @@
+package seleniumcourse;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class grid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
