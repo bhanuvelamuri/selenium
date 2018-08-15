5ef92b431b193e29a858b420c4de1fb1dcc494f6
bhanu velamuri <bhanu@dal.ca>
2018-08-15 00:06:52 -0300

    code for connecting node

--- course/src/seleniumcourse/grid.java
+++ course/src/seleniumcourse/grid.java
@@ -1,11 +1,26 @@
 package seleniumcourse;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class grid {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		// TODO Auto-generated method stub
+		DesiredCapabilities dc= new DesiredCapabilities();
+		dc.setBrowserName("Chrome");
+		dc.setPlatform(Platform.WINDOWS);
+		//dc.setVersion(IE9);
+		//dc.setPlatform(Platform.MAC);
+		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"),dc);
+		driver.get("http://rediff.com");
+		
 		
 	}
 
