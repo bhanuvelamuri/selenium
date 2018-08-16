f6cc17aed98b695779e00ac0e2c36fbd740e3b39
bhanu velamuri <bhanu@dal.ca>
2018-08-16 04:04:15 -0300

    using DataProviders technique

--- TestNG/src/test/parameters.java
+++ TestNG/src/test/parameters.java
@@ -1,8 +1,8 @@
 package test;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 public class parameters {
 
 @Parameters({"URL","APIKey/usrname"})
@@ -13,5 +13,33 @@ public void Website(String carloan, String key)
 	System.out.println(carloan);
 	System.out.println(key);
 }
+@Test(dataProvider="getData")
+public void users(String username,String password)
+{
+	//selenium
+	System.out.println("printing data");
+
+	System.out.println(username);
+
+	System.out.println(password);
+	
+}
+@DataProvider
+public Object[][] getData() {
+	//list combination-username password - good credit history
+	//user name and password - no credit history
+	//bad credit history
+	Object[][] data = new Object[3][2];
+	data[0][0]="first username";//first row first column
+	data[0][1]="password";//first row second column
+	data[1][0]="second username";//second row first column
+	data[1][1]="password";//second row second column
+	data[2][0]="third username";//third row first column
+	data[2][1]="password";//third row second column
+	return data;
+	
+}
+
 
 }
+ 
